258cb43a8d0a1fc6840ba1023ed95f0a21f10451
bryanaknight <brknig11@gmail.com>
2016-01-15 14:38:45 -0500

    remember session for logged in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128185032) do
+ActiveRecord::Schema.define(version: 20160115185851) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151128185032) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
